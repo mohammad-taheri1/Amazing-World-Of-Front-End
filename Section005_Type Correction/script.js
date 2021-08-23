0a20986109a3f9afcbddd445df60366a47542df1
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-24 00:57:24 +0430

    Section005_Type Correction

---
+++ Section005_Type Correction/script.js
@@ -0,0 +1,29 @@
+// var mStr = 'Hello'
+// var mNum = 5
+// var mSum = mStr + mNum
+
+// console.log(mSum)
+// console.log(typeof(mSum))
+
+// console.log(50 + 'hola')
+// console.log(typeof(50 + 'hola'))
+
+// var mValue = 2>='2'
+// console.log(mValue)
+// console.log(typeof(mValue))
+
+// var mNum = 15
+
+// console.log(typeof(String(mNum)))
+// console.log(typeof(String(true)))
+// console.log(typeof(String(-98.10)))
+// console.log(typeof(String(null)))
+// console.log(typeof(String(undefined)))
+
+// console.log(typeof(Number('2')))
+// console.log(typeof(Number('true')))
+
+console.log(typeof(Boolean('null')))
+
+
+
