2cdddc3bf6b6266991854040eb6149601cab6272
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-24 00:31:57 +0430

    Section004_Operators

---
+++ Section004_Operators/script.js
@@ -0,0 +1,68 @@
+//  +
+//  -
+//  *
+//  /
+//  %
+//  ++
+//  --
+
+// var num1 = 10;
+// var num2 = 15;
+// var num3 = 20;
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+
+// console.log(num1++)
+// console.log(++num1)
+// console.log(num1--)
+// console.log(--num1)
+
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+
+// var num1 = 15;
+// var num2 = 30;
+
+// console.log(num1+=10)
+// console.log(num1-=10)
+// console.log(num1*=10)
+// console.log(num1/=10)
+// console.log(num1%=10)
+
+// if (5 == "5") console.log("true");
+
+// if (5 === "5") {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// if (5 != "5") {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// if (5 !== "5") {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// if (15 < 15) { console.log("true"); } else { console.log("false"); }
+
+// if (15 <= 15) { console.log("true"); } else { console.log("false"); }
+
+// if ( 5 < 10 && 10 > 15) { console.log("true"); } else { console.log("false"); }
+
+// if ( 5 < 10 || 10 > 15) { console.log("true"); } else { console.log("false"); }
+
+2 < 3 ? console.log(true) : console.log(false)
