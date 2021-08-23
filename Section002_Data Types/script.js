1228d92cacb208d946b0fc0d56609cb134c5dc52
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-23 21:43:42 +0430

    Section002_Data Types

---
+++ Section002_Data Types/script.js
@@ -0,0 +1,132 @@
+// var num1 = 10;
+// var num2 = 10.9;
+
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+
+// var num3 = 15;
+// var sum = num1 + num2 + num3;
+// console.log(sum);
+
+// var diff = num3 - num1;
+// console.log(diff);
+
+// var mul = num3 * num1;
+// console.log(mul);
+
+// var div = num3 / num1;
+// console.log(div);
+
+// var divByZero = num3 / 0;
+// console.log(divByZero);
+// console.log(typeof(divByZero));
+
+// var mulByString = num3 * 'A';
+// console.log(mulByString);
+// console.log(typeof(mulByString));
+
+// Numbers Functions
+
+// var num1 = 10;
+// var num2 = 20.3;
+
+// console.log(num1.toString());
+// console.log(num2.toString());
+
+// var strNum1 = "10";
+// var strNum2 = "11.5";
+// var strNum3 = "ABC";
+
+// console.log(parseInt(strNum1));
+// console.log(parseInt(strNum2));
+// console.log(parseInt(strNum3));
+
+// console.log(parseFloat(strNum2));
+// console.log(parseFloat(strNum3));
+
+// var strFloat = 87.98768652265;
+// console.log(strFloat.toFixed());
+// console.log(strFloat.toFixed(2));
+// console.log(strFloat.toFixed(5));
+
+// Strings Functions
+
+// var myFirstString = "I Love Javacript"
+// console.log(typeof(myFirstString))
+
+// var mySecondString = 'We learn Javacript together'
+// console.log(mySecondString)
+
+// var doubleQuoteString = "This is a \"JavaScript\" string"
+// console.log(doubleQuoteString);
+
+// var singleQuoteString = 'This is a \'JavaScript\' string'
+// console.log(singleQuoteString);
+
+// var doubleQuoteString2 = "This is a 'JavaScript' string"
+// console.log(doubleQuoteString2);
+
+// var singleQuoteString2 = 'This is a "JavaScript" string'
+// console.log(singleQuoteString2);
+
+
+// var myFirstString = "This is a string for Javascript string functions, Javascript"
+
+// console.log(myFirstString.length)
+
+// console.log(myFirstString.indexOf('Javascript'))
+// console.log(myFirstString.indexOf('This'))
+
+// console.log(myFirstString.lastIndexOf('Javascript'))
+// console.log(myFirstString.lastIndexOf('This'))
+
+// console.log(myFirstString.slice(0, 4))
+// console.log(myFirstString.slice(21, 31))
+// console.log(myFirstString.slice(-10))
+// console.log(myFirstString.slice(5))
+
+// console.log(myFirstString.substr(0, 4))
+// console.log(myFirstString.substr(21, 10))
+// console.log(myFirstString.substr(21))
+
+// var exampleString = "This is Javascript tutorial"
+
+// console.log(exampleString.toUpperCase());
+// console.log(exampleString.toLowerCase());
+
+// var firstName = "Javascript"
+// var lastName = "Playground"
+
+// console.log(exampleString.concat(firstName));
+// console.log(exampleString.concat(' ',firstName, ' ', lastName));
+
+// console.log(firstName + ' ' + lastName + ' ' + exampleString)
+
+// var extraSpaceString = '                  mystring                '
+// console.log(extraSpace.trim());
+
+// var extraSpaceString2 = '                  my string                '
+// console.log(extraSpaceString2.trim());
+
+// var charAtExampleString = 'This is my test string'
+// console.log(charAtExampleString.charAt(5));
+// console.log(charAtExampleString.charAt(11));
+
+// var sampleString = "This is my sample string"
+
+// console.log(sampleString.split(' '));
+// console.log(sampleString.split(' ').length);
+
+// var sampleString2 = "This,is,my,sample,string"
+// console.log(sampleString2.split(','));
+
+// var sampleString3 = "This is awesome"
+// console.log(sampleString3.split());
+
+// Null and Undefined
+
+var myVar
+console.log(myVar)  //  -> undefined ~ no value
+
+myVar = null
+console.log(myVar)  //  -> null ~ it is a value of null
