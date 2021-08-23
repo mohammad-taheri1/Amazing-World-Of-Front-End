b6b9f51ed21b239452de14623d9869d52077df5c
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-23 20:37:53 +0430

    Section001_Variables

---
+++ Section001_Variables/script.js
@@ -0,0 +1,40 @@
+// var num1 = 5;
+// var num2 = 10;
+// var num3 = 15;
+
+// var sum = num1 + num2 + num3;
+
+// console.log(sum);
+
+// ************************************************************************************************
+
+var firstNum = 10; 
+console.log(firstNum);
+
+var secondNum = 15;
+
+console.log(secondNum);
+
+// Camel case   (Recommended for JS)
+// var firstNum;
+// Pascal case
+// var FirstNum;
+// Snake case
+// var first_num;
+
+// Rules for naming JS variables 
+
+// + letters, digits, _ and $
+// var mySubject1, math$; 
+
+// - start with digit
+// var 1maths; 
+
+// + start with _ or $
+// var _subjectmarks, $rankClass;
+
+// + case sensitive
+// var x, X;
+
+// - reserved keywords
+// var var;
