6099229aff191258aa4bc7c5651c8c8ab0864d09
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-24 23:13:45 +0430

    Section010_Browser Object Mode

---
+++ Section010_Browser Object Mode/script.js
@@ -0,0 +1,31 @@
+// Bom
+
+// console.log(window.document.getElementById('add-item')) 
+// console.log(document.getElementById('add-item')) 
+
+// console.log(window.innerWidth)
+// console.log(window.innerHeight)
+
+// window.alert('ok?')
+
+// console.log(confirm('Are you sure?'))
+// console.log(prompt('Enter your name please','Unknown'))
+
+// window.open('https://www.google.com')
+
+// close()
+
+// console.log(window.screen)
+// console.log(window.navigator.userAgent)
+// console.log(window.navigator.onLine)
+
+// console.log(window.history)
+// console.log(window.history.back())
+// console.log(window.history.forward())
+
+// console.log(window.location)
+// console.log(window.location.href)
+// console.log(window.location.hostname)
+// console.log(window.location.pathname)
+
+window.location.assign('https://www.google.com')
