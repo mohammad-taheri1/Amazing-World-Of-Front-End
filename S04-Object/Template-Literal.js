b8949272c6c1483bcc1c14062d8419cdd98e30b4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-08 21:26:09 +0330

    S04

---
+++ S04-Object/Template-Literal.js
@@ -0,0 +1,9 @@
+let message = 'This\'s a message for O\'kinva \n' + 'Hopkins';
+
+let message2 = `This's a message for O'kinva 
+                opkins`;
+
+
+function handleName(name){
+    console.log(`Welcome Mr/Mrs ${name}`)
+}
