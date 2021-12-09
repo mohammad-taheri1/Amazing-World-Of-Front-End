dfd0b034be0147093fd8b6a2f277c002a397cf36
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 00:25:07 +0330

    working with functions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // function decleration
-function talk(){
-    console.log("salam mamad");
-}
+// function talk(){
+//     console.log("salam mamad");
+// }
 
 // function expression
-const run = function (){
-    console.log("salam mamad");
-}
+// const run = function (){
+//     console.log("salam mamad");
+// }
 
 // arguments, rest operator
 
