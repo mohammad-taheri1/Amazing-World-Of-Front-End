ed4fce094a0203f673a393c64670066d68c7cf76
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-09 14:17:10 +0330

    S05

--- S05-Arrays/Array.js
+++ S05-Arrays/Array.js
@@ -23,7 +23,7 @@
 
 // console.log(data.findIndex(q => q.id === 2));
 
-const first = [3, 15, 9, 8, 15, 6];
+// const first = [3, 15, 9, 8, 15, 6];
 
 // const second = [4,5,6];
 
@@ -31,4 +31,23 @@ const first = [3, 15, 9, 8, 15, 6];
 
 // console.log(mix)
 
-console.log(first.slice(0, 3))
+// console.log(first.slice(0, 3))
+
+// const mixed = [...first, 653, 6585];
+// console.log(mixed);
+
+// const message = "salam mamad taheri";
+//  const splitted = message.split(" ");
+//  const joinSplitted = splitted.join("-");
+// console.log(joinSplitted);
+
+// const newMessage = message.split(" ").join("-");
+
+const numbers = [3, 15, 6, -1, 15, 6];
+
+const checkEvery = numbers.every(q => {
+    console.log(q ,  q>0);
+    return q>0;
+});
+
+console.log(checkEvery)
