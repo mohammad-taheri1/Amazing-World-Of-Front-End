4892867e7d4d16ffa79896deafbf83cde090d987
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-09 13:46:57 +0330

    S05

--- S05-Arrays/Array.js
+++ S05-Arrays/Array.js
@@ -12,11 +12,23 @@
 // console.log(numbers.indexOf(18));
 // console.log(numbers.includes(17));
 
-const data = [
-    {name: "a", id: 1},
-    {name: "b", id: 2},
-];
+// const data = [
+//     {name: "a", id: 1},
+//     {name: "b", id: 2},
+// ];
+//
+// console.log(data.slice(1,));
 
-console.log(data.indexOf(data.find(q  => q.id === 2)));
+// console.log(data.indexOf(data.find(q  => q.id === 2)));
 
-console.log(data.findIndex(q => q.id === 2));
+// console.log(data.findIndex(q => q.id === 2));
+
+const first = [3, 15, 9, 8, 15, 6];
+
+// const second = [4,5,6];
+
+// const mix = first.concat(second);
+
+// console.log(mix)
+
+console.log(first.slice(0, 3))
