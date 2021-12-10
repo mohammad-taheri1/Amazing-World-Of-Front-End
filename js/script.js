6d7734fe5adae7578ea917044b40f56fa8be28c4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 13:43:46 +0330

    working with DOM

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
-const courses = [
-    {id: 1, title: "Product 1", isVisible: true},
-    {id: 2, title: "Product 2", isVisible: true},
-    {id: 3, title: "Product 3", isVisible: false},
-    {id: 4, title: "Product 4", isVisible: true},
-];
+const addBtn = document.querySelector("button");
+const pS = document.querySelectorAll("p");
 
-const visibleProducts = courses.filter(q => q.isVisible);
-
-const msg = document.createElement("h3");
-msg.textContent = `${visibleProducts.length} Products are visible`;
-const body = document.querySelector("body");
-body.appendChild(msg)
-
-visibleProducts.forEach(q => {
-    const p = document.createElement("p");
-    p.textContent = `id: ${q.id} title: ${q.title}`;
-    console.log(p)
-    body.appendChild(p);
+addBtn.addEventListener("click", (e) => {
+    pS.forEach(item => {
+        item.remove();
+    })
 });
