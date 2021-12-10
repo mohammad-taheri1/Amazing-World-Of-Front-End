947721a2edfe99addbd12a4c6d696758df0598c4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 12:44:34 +0330

    working with DOM

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-const pS = document.querySelectorAll("p");
+const courses = [
+    {id: 1, title: "Product 1", isVisible: true},
+    {id: 2, title: "Product 2", isVisible: true},
+    {id: 3, title: "Product 3", isVisible: false},
+    {id: 4, title: "Product 4", isVisible: true},
+];
 
-pS.forEach((item, index) => {
-    if (item.textContent.includes("3")) item.remove();
+const visibleProducts = courses.filter(q => q.isVisible);
+
+const msg = document.createElement("h3");
+msg.textContent = `${visibleProducts.length} Products are visible`;
+const body = document.querySelector("body");
+body.appendChild(msg)
+
+visibleProducts.forEach(q => {
+    const p = document.createElement("p");
+    p.textContent = `id: ${q.id} title: ${q.title}`;
+    console.log(p)
+    body.appendChild(p);
 });
-
-const p = document
-.createElement("p");
-p.textContent = "Mohammad new P";
-document.querySelector("body").append(p);
