900a3680769c36dc5979c324c000f6bc420ffd7d
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 23:52:36 +0330

    extend async sample

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 console.log("start");
 
 setTimeout(()=>{
-    console.log("duration")
+    console.log("2 mili seconds later")
 }, 2000);
+
+setTimeout(()=>{
+    console.log("1 mili seconds later")
+}, 1000);
+
+setTimeout(()=>{
+    console.log("0 mili seconds later")
+}, 0);
 
 console.log("end");
