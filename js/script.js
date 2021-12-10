ec22f2a8ca1d408aaafaf721bf54a9394143c573
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 23:19:45 +0330

    getter and setter

--- js/script.js
+++ js/script.js
@@ -4,16 +4,22 @@ class Rectangle{
         this.y = _y;
         this.color = _color;
     }
-    getArea(){
+    get area(){
         return this.x * this.y;
+    }
+    set area(a){
+        this.x = Math.sqrt(a);
+        this.y = this.x;
     }
     printDescription(){
         console.log(`this rectangle has ${this.x} width and ${this.y} height`)
     }
+
 }
 
 const testRectangle1 = new Rectangle(20,25,"blue");
 const testRectangle2 = new Rectangle(35,80,"red");
 
-testRectangle1.printDescription();
-testRectangle2.printDescription();
+console.log(testRectangle1.x);
+testRectangle1.area = 950;
+console.log(testRectangle1.x);
