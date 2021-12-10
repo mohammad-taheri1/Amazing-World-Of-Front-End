abc78298898a140279f92ebfe24e682612947c15
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 14:56:42 +0330

    refactor

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-const incBtn = document.querySelector("#inc");
-const decBtn = document.querySelector("#dec");
-const resetBtn = document.querySelector("#reset");
+const btnS = document.querySelectorAll(".btn");
 const counter = document.querySelector("#counter");
 
 let count = 0;
 
-incBtn.addEventListener("click",()=>{
-    count++;
-    counter.textContent = count;
-});
+// incBtn.addEventListener("click",()=>{
+//     count++;
+//     counter.textContent = count;
+// });
+//
+// decBtn.addEventListener("click",()=>{
+//     count--;
+//     counter.textContent = count;
+// });
+//
+// resetBtn.addEventListener("click",()=>{
+//     count = 0;
+//     counter.textContent = count;
+// });
 
-decBtn.addEventListener("click",()=>{
-    count--;
-    counter.textContent = count;
-});
-
-resetBtn.addEventListener("click",()=>{
-    count = 0;
-    counter.textContent = count;
-});
+btnS.forEach(btn => {
+    btn.addEventListener("click", ()=> {
+        const classes = btn.classList;
+        if(classes.contains("inc")) count++;
+        else if(classes.contains("dec")) count--;
+        else count = 0;
+        counter.textContent = count;
+        if(count > 0) counter.style.color = "blue";
+        else counter.style.color = "red";
+    })
+})
