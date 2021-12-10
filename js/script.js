f550d874fc2b81fd49a6b24a9592473b54a4e3fb
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 22:56:46 +0330

    working with classess

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-const showbtn = document.querySelector(".show-modal");
-const modal = document.querySelector(".modal");
-const backDrop = document.querySelector(".backdrop");
-const closebtn = document.querySelector(".close-modal");
+class Rectangle{
 
-showbtn.addEventListener("click",()=>{
-    modal.style.opacity = "1";
-    modal.style.transform = "translateY(20vh)"
-    backDrop.style.display = "block";
-})
-
-closebtn.addEventListener("click",()=>{
-    modal.style.opacity = "0";
-    modal.style.transform = "translateY(-100vh)"
-    backDrop.style.display = "none";
-})
-
+}
