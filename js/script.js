716432ca041c82df1bdc1c419e1ec6f9bdd039d6
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 14:40:30 +0330

    working with DOM

--- js/script.js
+++ js/script.js
@@ -1,30 +1,21 @@
-const searchInput = document.querySelector("#search");
+const incBtn = document.querySelector("#inc");
+const decBtn = document.querySelector("#dec");
+const resetBtn = document.querySelector("#reset");
+const counter = document.querySelector("#counter");
 
-const products = [
-    {id: 1, title: "new producsf sgsg dsgasdgds fgdsfG"},
-    {id: 2, title: "SADFJK SDAGNDSJ WEP 5DGSA"},
-    {id: 3, title: "DDSFA EEW FHS SFGAS "},
-    {id: 4, title: "ASDF RGRL  'RTOPJRWOIGERWO"},
-    {id: 5, title: "DFAS L;GIO JRNGUPOAS"},
-    {id: 6, title: "LOIEWUB IWRHGUISDLV;"},
-    {id: 7, title: "DSALKFJB OIWRGIB./ KLRGNU KJAFNUODVI "},
-    {id: 8, title: "LSJDF URGUIRG URGPFO OEWIIFH; UD GF"},
-    {id: 9, title: "DSLKF KEWGFOI UIREGUI OF;DVDS5"},
-    {id: 10, title: "DS DVC FE FIOOI RJGNRUI "},
-];
+let count = 0;
 
-const filterString={
-    searchItem : '',
-}
+incBtn.addEventListener("click",()=>{
+    count++;
+    counter.textContent = count;
+});
 
-function renderProducts(products, filterString){
-    const result = products.filter(q => q.title.toLowerCase().includes(filterString.searchItem.toLowerCase()));
-    console.log(result);
-}
+decBtn.addEventListener("click",()=>{
+    count--;
+    counter.textContent = count;
+});
 
-
-
-searchInput.addEventListener("input", (e) => {
-     filterString.searchItem = e.target.value;
-     renderProducts(products, filterString);
-})
+resetBtn.addEventListener("click",()=>{
+    count = 0;
+    counter.textContent = count;
+});
