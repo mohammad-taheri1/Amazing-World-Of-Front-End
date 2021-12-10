fd039d24db5b131a6b8847b3df2d3d87478f857a
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 14:14:34 +0330

    working with DOM

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-const addBtn = document.querySelector("button");
-const pS = document.querySelectorAll("p");
-
-addBtn.addEventListener("click", (e) => {
-    pS.forEach(item => {
-        item.remove();
-    })
-});
-
 const searchInput = document.querySelector("#search");
 
-searchInput.addEventListener('change', (e)=>{
-    console.log(e.target.value);
-});
+const products = [
+    {id: 1, title: "new producsf sgsg dsgasdgds fgdsfG"},
+    {id: 2, title: "SADFJK SDAGNDSJ WEP 5DGSA"},
+    {id: 3, title: "DDSFA EEW FHS SFGAS "},
+    {id: 4, title: "ASDF RGRL  'RTOPJRWOIGERWO"},
+    {id: 5, title: "DFAS L;GIO JRNGUPOAS"},
+    {id: 6, title: "LOIEWUB IWRHGUISDLV;"},
+    {id: 7, title: "DSALKFJB OIWRGIB./ KLRGNU KJAFNUODVI "},
+    {id: 8, title: "LSJDF URGUIRG URGPFO OEWIIFH; UD GF"},
+    {id: 9, title: "DSLKF KEWGFOI UIREGUI OF;DVDS5"},
+    {id: 10, title: "DS DVC FE FIOOI RJGNRUI "},
+];
+
+const filterString={
+    searchItem : '',
+}
+
+function renderProducts(products, filterString){
+    const result = products.filter(q => q.title.toLowerCase().includes(filterString.searchItem.toLowerCase()));
+    console.log(result);
+}
+
+
+
+searchInput.addEventListener("input", (e) => {
+     filterString.searchItem = e.target.value;
+     renderProducts(products, filterString);
+})
