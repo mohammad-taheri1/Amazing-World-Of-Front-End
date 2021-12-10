e7024e3f63ca3456519223613fa8a87e0467b7f0
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 23:43:49 +0330

    Polymorphism

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
-class Person{
-    constructor(_name,_age) {
-        this.name=_name;
-        this.age = _age;
+class Animal{
+    constructor(_name) {
+        this.name = _name;
     }
-    introduce(){
-        console.log(`The Person name is ${this.name} and his/her age is ${this.age}`)
-    }
-}
-
-class Programmer extends Person{
-    constructor(_name,_age,_experties) {
-        super(_name,_age);
-        this.expertis = _experties;
-    }
-    introduce() {
-        console.log("override")
-    }
-
-    code(){
-        console.log(`${this.name} has experty of ${this.expertis}`);
+    eats(){
+        console.log(`the ${this.name} eats food`);
     }
 }
 
-const person1 = new Person("mamad", 32);
+class Dog extends Animal{
+    eats() {
+        super.eats();
+        console.log(`overrided function`);
+    }
 
-const mamad = new Programmer("mamad", 32, "react Developer")
+}
+
+const animal1 = new Dog('animaltest');
+animal1.eats();
