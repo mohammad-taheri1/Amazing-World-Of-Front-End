2219e697f9ceb3f035464ca75d437de897af66e0
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-11 00:06:32 +0330

    callback

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
+// console.log("start");
+//
+// setTimeout(()=>{
+//     console.log("2 mili seconds later")
+// }, 2000);
+//
+// setTimeout(()=>{
+//     console.log("1 mili seconds later")
+// }, 1000);
+//
+// setTimeout(()=>{
+//     console.log("0 mili seconds later")
+// }, 0);
+//
+// console.log("end");
+
+// **********************************************
+
 console.log("start");
 
-setTimeout(()=>{
-    console.log("2 mili seconds later")
-}, 2000);
-
-setTimeout(()=>{
-    console.log("1 mili seconds later")
-}, 1000);
-
-setTimeout(()=>{
-    console.log("0 mili seconds later")
-}, 0);
+function loginUser(email, password, callback){
+    setTimeout(()=>{
+        console.log("data is ready");
+        callback({ email});
+    },3000);
+}
+loginUser("mamad@hmail.com", 11111,(user)=>{
+    console.log(user);
+});
 
 console.log("end");
