3d113d8b6bf3055fcc43582d0d1781c8ac1ab931
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 23:09:57 +0330

    working with classess

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 class Rectangle{
-
+    constructor(_x,_y,_color) {
+        this.x = _x;
+        this.y = _y;
+        this.color = _color;
+        console.log(this)
+    }
 }
+
+const testRectangle1 = new Rectangle(20,25,"blue");
+const testRectangle2 = new Rectangle(35,80,"red");
