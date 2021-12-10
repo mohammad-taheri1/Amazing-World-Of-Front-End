8ef4686509be397276f94fec7c2ba3490a88d63f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-10 12:36:35 +0330

    working with DOM

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ const pS = document.querySelectorAll("p");
 pS.forEach((item, index) => {
     if (item.textContent.includes("3")) item.remove();
 });
+
+const p = document
+.createElement("p");
+p.textContent = "Mohammad new P";
+document.querySelector("body").append(p);
