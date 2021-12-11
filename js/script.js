0212ccbfb656b15223bbfaf64b8af10b55fc9f56
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-11 21:30:19 +0330

    axios get && post

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ const getBtn = document.querySelector(".get-data");
 const postBtn = document.querySelector(".post-data");
 
 function getData(){
-    fetch("https://jsonplaceholder.typicode.com/users/5", {method: "GET"})
-        .then(response => {
-             return response.json();
-        })
-        .then(data => console.log(data))
+    // fetch("https://jsonplaceholder.typicode.com/users", {method: "GET"})
+    //     .then(response => {
+    //          return response.json();
+    //     })
+    //     .then(data => console.log(data))
+    axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(response => console.log(response.data));
 }
 
 function postData(){
@@ -16,17 +18,19 @@ function postData(){
         username: "mamad@mamad.mamad",
         email: "mamad@gmail.com",
     };
-    fetch("https://jsonplaceholder.typicode.com/users", {
-        method: "POST",
-        body:JSON.stringify(userData),
-        headers: {
-                'content-type': 'application/jspn'
-            },
-    })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => console.log(data))
+    // fetch("https://jsonplaceholder.typicode.com/users", {
+    //     method: "POST",
+    //     body:JSON.stringify(userData),
+    //     headers: {
+    //             'content-type': 'application/jspn'
+    //         },
+    // })
+    //     .then(response => {
+    //         return response.json();
+    //     })
+    //     .then(data => console.log(data))
+    axios.post("https://jsonplaceholder.typicode.com/users", userData)
+        .then(response => console.log(response.data));
 }
 
 getBtn.addEventListener("click",getData);
