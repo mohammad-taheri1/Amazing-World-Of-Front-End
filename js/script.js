a739198109f0aea4995af7866ecba1c64c4bbee5
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-21 00:10:02 +0330

    training

--- js/script.js
+++ js/script.js
@@ -12,3 +12,27 @@ var num1 = 5;
 num1++;
 num1--;
 console.log(num1);
+
+// **********************************************
+
+// Create Function
+function testFunction(){
+    console.log("salam mamad");
+}
+
+// Invoke (Call) Function
+testFunction();
+
+function greeting(){
+    var _name = prompt('What is your name ?');
+    var result = 'Hello ' + _name;
+    console.log(result);
+}
+
+greeting();
+
+function sumNumbers(num1, num2){
+    console.log(num1 + num2);
+}
+
+sumNumbers(4, 13);
