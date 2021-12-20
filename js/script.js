6c66bed7b982ba95cae1cc52a6d3862b58fe5f3a
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-20 23:59:29 +0330

    training

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var _name = "mamad";
+
+console.log(_name);
+
+var user_age = prompt("What is your age? ");
+
+document.getElementById("body").innerHTML = user_age;
+
+// **********************************************
+
+var num1 = 5;
+num1++;
+num1--;
+console.log(num1);
