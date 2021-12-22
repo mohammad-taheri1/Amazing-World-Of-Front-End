c4eafde24efdec781cbf58b5e51de89fa72ab363
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-22 23:29:49 +0330

    some challenges

--- js/app.js
+++ js/app.js
@@ -34,13 +34,51 @@
 // console.log(getFFirstItem1(items));
 // console.log(getFFirstItem1(items));
 
-function badOrGoodMovie(movie_rating){
-    if(movie_rating >= 7) {
-        return ("Good Movie");
-    } else {
-        return("Bad Movie");
+// function badOrGoodMovie(movie_rating){
+//     if(movie_rating >= 7) {
+//         return ("Good Movie");
+//     } else {
+//         return("Bad Movie");
+//     }
+// }
+//
+// console.log(badOrGoodMovie(3))
+// console.log(badOrGoodMovie(9))
+
+// function isEmptyString(some_string){
+//     return !!some_string.length;
+// }
+//
+// console.log(isEmptyString(""));
+// console.log(isEmptyString("mamad"));
+
+// function findMinium(listOfNumbers){
+//     return (Math.min(...listOfNumbers))
+// }
+// function findMax(listOfNumbers){
+//     return (Math.max(...listOfNumbers))
+// }
+//
+// const numbers = [5,65,2,14,36,895,21,44]
+// console.log(findMinium(numbers));
+// console.log(findMax(numbers));
+
+function sortBestRatingFirst(numbers) {
+    for(let j = 0; j < numbers.length-1; j++){
+    let max_num = numbers[j];
+    let max_location = j;
+    for (let i = j; i < numbers.length; i++) {
+        if (numbers[i] > max_num) {
+            max_num = numbers[i];
+            max_location = i;
+        }
     }
+    numbers[max_location] = numbers[j];
+    numbers[j] = max_num;
+    }
+    return  numbers;
 }
 
-console.log(badOrGoodMovie(3))
-console.log(badOrGoodMovie(9))
+let arr = [5,8,2,9,3,10];
+
+console.log(sortBestRatingFirst(arr))
