f1ca4f07b70f569dd46784512713c0ce0fb49ef5
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-24 19:27:55 +0330

    handle alignText

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * DONE: Update the text in the "Formatted Text" section as a user types in the textarea
  * DONE TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
  * DONE: Add an .underline class to "Formatted Text" when Underline button is clicked
- * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
+ * DONE: Toggle the align style for "Formatted Text" when the appropriate button is clicked
  */
 
 
@@ -62,4 +62,11 @@
    */
   function alignText(elem, alignType){
     // CODE GOES HERE
+    elem.classList.toggle('active');
+    document.getElementById("text-output").style.textAlign = alignType;
+    let buttonsList = document.getElementsByClassName("align");
+    for(let i = 0; i< buttonsList.length; i++) {
+      buttonsList[i].classList.remove("active");
+    }
+    elem.classList.add('active')
   }
