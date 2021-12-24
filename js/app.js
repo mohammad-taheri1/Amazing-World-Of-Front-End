81e7caf3675779eb7921c8d1c32ce7b37879fa62
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-24 18:20:17 +0330

    init

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const titr = document.querySelector(".titr");
-let newTitrElement = document.createElement("h3");
-newTitrElement.innerText = "ممد";
-newTitrElement.className = "text-white bg-danger";
-titr.append(newTitrElement);
-titr.prepend(newTitrElement);
