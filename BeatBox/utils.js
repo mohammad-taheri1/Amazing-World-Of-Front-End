6d3529879f712b85298d5272a308352a718e03d5
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-25 17:11:51 +0330

    beat-box init

---
+++ BeatBox/utils.js
@@ -0,0 +1,35 @@
+/**
+ * Beat class that keeps track of playing the audio
+ * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
+ * HINT: Create a play function to play the audio if called 
+ */
+class Beat {
+}
+
+
+
+/**
+ * Button class that keeps track of the button color based on a press
+ */
+class Button {
+    constructor(color, keyCode){
+    }
+
+    /**
+     * Set the button color based on color specified
+     */
+    setButtonColorInHTML = () => {
+    }
+
+    /**
+     * Select function to set the background color and boxShadow
+     */
+    select = () => {
+    }
+
+    /**
+     * Deselect function to reset background color and boxShadow
+     */
+    deselect = () => {
+    }
+}
