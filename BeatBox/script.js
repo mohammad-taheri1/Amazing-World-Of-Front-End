fb986ba23c1bdd4ab96003a7c1da6978fa1e372c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-25 18:28:06 +0330

    update

--- BeatBox/script.js
+++ BeatBox/script.js
@@ -8,7 +8,7 @@
  * DONE: Button pt4: Call the select() function upon key press ;)
  * DONE: Button pt5: Add transition for button selection
  * DONE: Button pt6: Remove the button style upon transition end | Use deselect function
- * TODO: Complete all button instances with the following colors
+ * DONE: Complete all button instances with the following colors
  * TODO: Add background image
  * First 3: #00fffe
  * 4,5,6,7: #FF00FF
@@ -22,27 +22,35 @@ let beats = {
     },
     "83": {
         beat: new Beat("./assets/Piano Chord 209.mp3"),
+        button: new Button("#00fffe", 83)
     },
     "68": {
         beat: new Beat("./assets/Piano Chord 208.mp3"),
+        button: new Button("#00fffe", 68)
     },
     "70": {
         beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+        button: new Button("#FF00FF", 70)
     },
     "71": {
         beat: new Beat("./assets/Drum Snare Roll.mp3"),
+        button: new Button("#FF00FF", 71)
     },
     "72": {
         beat: new Beat("./assets/PREL Musical 57.mp3"),
+        button: new Button("#FF00FF", 72)
     },
     "74": {
         beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+        button: new Button("#FF00FF", 74)
     },
     "75": {
         beat: new Beat("./assets/Musical Compos 33.mp3"),
+        button: new Button("#FFFFFF", 75)
     },
     "76": {
         beat: new Beat("./assets/Musical Orches 4.mp3"),
+        button: new Button("#FFFFFF", 76)
     }
 }
 
