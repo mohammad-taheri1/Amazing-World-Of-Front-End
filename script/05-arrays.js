de58b6292a2702f7554e7d22f7f8cc1d082a3035
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-16 17:45:23 +0330

    update

--- script/05-arrays.js
+++ script/05-arrays.js
@@ -4,3 +4,6 @@ const nums = [10, 20, 30, 40, 50, 60, 70 ,80, 90, 100];
 names.forEach((q, i) => console.log(i, " -> ", q));
 console.log("*******************************")
 nums.forEach((q, i) => console.log(i, " -> ", q));
+
+console.log(names);
+console.log(nums);
