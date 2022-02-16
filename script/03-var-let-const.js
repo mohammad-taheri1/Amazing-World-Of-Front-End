50d30b9ccb68937068cb2c165dcae5a6f15e9bac
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-16 15:42:38 +0330

    train

---
+++ script/03-var-let-const.js
@@ -0,0 +1,92 @@
+// console.log(x);
+// let x;
+// x = 10;
+
+// try {
+//     console.log(sum(5, 10))
+// } catch{
+//     console.log("error ")
+// }
+
+// function decleration
+// function sum(a,b){
+//     return a + b;
+// }
+
+// function expression
+// const sum = function (a,b){
+//     return a + b;
+// }
+
+// let x = 10;
+// let y = "20"
+// console.log("x + y =", x + y);
+// console.log("x * y =", x * y);
+// console.log("x / y =", x / y);
+// console.log("x - y =", x - y);
+
+// let x = 10;
+// let y = "ali"
+// console.log("x + y =", x + y);
+// console.log("x * y =", x * y);
+// console.log("x / y =", x / y);
+// console.log("x - y =", x - y);
+
+// console.log(typeof (x * y))
+
+// const sample = x * y;
+
+// if (isNaN(sample)){
+//     console.log("Naan")
+// }
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+// console.log(i)
+// console.log("**********************")
+// for (let j = 0; j < 10; j++) {
+//     console.log(j)
+// }
+// console.log(j)
+
+// function test(){
+//     var x = 10;
+// }
+// test();
+// console.log(x)
+
+// if (true){
+//     var y = 20;
+// }
+// console.log(y)
+
+// var x = 10;
+
+// function test(){
+//     console.log(x);
+//     var x = 20;
+//     console.log(x);
+// }
+// test();
+// console.log(x);
+// var x = 10;
+
+// function test() {
+//     console.log(x);
+//     let x = 20;
+//     console.log(x);
+//   }
+//   test();
+//   console.log(x);
+
+// try {
+
+ 
+// } catch (error) {
+//     console.log("متن ارور ->  ", error)
+// }
+
+// console.log("mammad")
+
+
