194d222cb8ff1480c3d6d26b5d3a711c7cc7857e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-01 01:01:06 +0330

    some closure codes

--- script/08-default-param.js
+++ script/08-default-param.js
@@ -1,7 +1,38 @@
-function sum(a = 1, b = 1){
-    return a + b;
-}
+// function sum(a = 1, b = 1){
+//     return a + b;
+// }
+//
+// console.log(sum(10, 3))
+// console.log(sum(10))
+// console.log(sum())
 
-console.log(sum(10, 3))
-console.log(sum(10))
-console.log(sum())
+
+// {
+//     // do some job with local variables that should not be seen outside
+//
+//     let message = "Hello"; // only visible in this block
+//
+//     alert(message); // Hello
+// }
+//
+// alert(message); // Error: message is not defined
+
+// {
+//     // show message
+//     let message = "Hello";
+//     alert(message);
+// }
+//
+// {
+//     // show another message
+//     let message = "Goodbye";
+//     alert(message);
+// }
+
+// // show message
+// let message = "Hello";
+// alert(message);
+//
+// // show another message
+// let message = "Goodbye"; // Error: variable already declared
+// alert(message);
