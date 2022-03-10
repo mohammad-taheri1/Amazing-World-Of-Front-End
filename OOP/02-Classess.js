a59506c757b31dacd13807459cf4e286abc34f54
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-10 16:42:17 +0330

    method chaning

--- OOP/02-Classess.js
+++ OOP/02-Classess.js
@@ -2,12 +2,31 @@ class User {
     constructor(_email, _name){
         this.email = _email;
         this.name = _name;
-        console.log('constructor has been invoked');
+        this.score = 0;
+        console.log('constructor has been invoked for', this.name);
     }
+    login(){
+        console.log(this.email, 'has logged in');
+        return this;
+    }
+    logout() {
+        console.log(this.email, 'has logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+
 }
 
  var userOne = new User('mamad@gmail.com', 'mamad');
  var userTwo = new User('alice@gmail.com', 'alice');
 
- console.log(userOne);
- console.log(userTwo);
+//  userOne.login();
+//  userOne.logout();
+//  userTwo.login();
+//  userTwo.logout();
+
+userOne.login().updateScore().updateScore().logout();
