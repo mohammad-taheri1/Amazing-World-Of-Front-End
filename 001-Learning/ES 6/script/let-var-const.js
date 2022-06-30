ef3d7cd76471c0195131f3eb6aad4eeed96c6c5c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-30 14:21:11 +0430

    add ES 6

---
+++ 001-Learning/ES 6/script/let-var-const.js
@@ -0,0 +1,24 @@
+// var i = 5;
+
+// var i = 6;
+
+// let j = 5;
+
+// let j = 6;
+
+// const k = 5;
+
+// const k = 6;
+
+if (true) {
+    var iii = 4;
+    console.log(iii)
+}
+
+if (true) {
+    let jjj = 5;
+    console.log(jjj)
+}
+
+console.log(iii)
+console.log(jjj)
